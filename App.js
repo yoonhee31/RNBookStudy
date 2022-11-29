0d3d77c4719ba97a2c469c386129474c292ef074
yoonhee31 <yhmelody31@gmail.com>
2022-11-29 23:21:28 +0900

    first commit

---
+++ App.js
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hiii yoooonhee!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
