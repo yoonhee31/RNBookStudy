c90f02b6d0ca2005a2c217e3d690b062858515ea
yoonhee31 <yhmelody31@gmail.com>
2022-11-30 16:25:34 +0900

    counter 만들기

--- App.js
+++ App.js
@@ -1,18 +1,29 @@
 import React, {useState} from 'react';
-import {Button, SafeAreaView} from 'react-native';
-import Box from './components/Box';
+import {Button, SafeAreaView, StyleSheet} from 'react-native';
+import Counter from './components/counter';
 
 const App = () => {
-  const [visible, setVisible] = useState(true);
-  const onPress = () => {
-    setVisible(!visible);
+  const [count, setCount] = useState(0);
+  const onIncrease = () => setCount(count => count + 1);
+  const onDecrease = () => {
+    if (count === 0) {
+      return 0;
+    } else {
+      setCount(count => count - 1);
+    }
   };
   return (
-    <SafeAreaView>
-      <Button title="토글" onPress={onPress} />
-      {visible && <Box rounded={true} size="large" color="blue" />}
+    <SafeAreaView style={styles.full}>
+      <Counter count={count} onIncrease={onIncrease} onDecrease={onDecrease} />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  full: {
+    flex: 1,
+    backgroundColor: 'pink',
+  },
+});
 
 export default App;
